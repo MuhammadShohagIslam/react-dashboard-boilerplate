c323de51dba47cf07386c0aeaceea4f628592f19
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-05-31 16:46:21 +0300

    react redux for auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,24 @@ import { useState, useEffect } from "react";
 import publicRoutes from "./router/routes/publicRoutes";
 import { getRoutes } from "./router/routes";
 import Router from "./router/Router";
+import { useDispatch, useSelector } from "react-redux";
+import { get_user_info } from "./store/reducers/authReducers";
 
 function App() {
     const [allRoutes, setAllRoutes] = useState(...publicRoutes);
-
+    const dispatch = useDispatch();
+    const { token } = useSelector((state) => state.auth);
+    
     useEffect(() => {
         const routes = getRoutes();
         setAllRoutes([...allRoutes, routes]);
     }, []);
+
+    useEffect(() => {
+        if (token) {
+            dispatch(get_user_info());
+        }
+    }, [token]);
 
     return <Router allRoutes={allRoutes} />;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
-import ReactDOM from 'react-dom/client'
-import './index.css'
-const App = lazy(() => import ('./App.jsx'))
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { store } from "./store/index";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const App = lazy(() => import("./App.jsx"));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-	<BrowserRouter>
-		<Suspense fallback='loading...'>
-			<App />
-		 </Suspense>
-	</BrowserRouter>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <Suspense fallback="loading...">
+                    <App />
+                    <Toaster />
+                </Suspense>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
